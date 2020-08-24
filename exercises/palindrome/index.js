ae5158c86e46e488a49260c9846dbe1884c1556f
Alice Jowsey <12345alcatraz@gmail.com>
2020-08-24 17:41:25 +1200

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,29 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
 
-module.exports = palindrome;
+//  solution 2 - not ideal but one way of solving 'an advanced array helper "every" ' - complex in array and mirroring & access to array;
+ function palindrome(str)  { //turn string to array every helper only has access to an array
+    return str.split('').every((char, i) => {     //str.split makes array | i=0 first element of array 
+        return char === str[str.length - i - 1];  //give access to length -1 of array other end, returns index at other sid eof array mirrored
+    }); 
+ }
+
+ module.exports = palindrome;
+
+
+
+//solution 1 reverses string to help
+// function palindrome(str)  {
+//     const reversed = str
+//     .split('')
+//     .reverse()
+//     .join('');
+
+//     return str === reversed;
+//  }
+
+// module.exports = palindrome;
+
+//double comparisons first to last or last to first I can propose a clear solution 
+//alternative solutions could be offered to interviewer
