64ca444b437b316cee1effa27d27d5b05da70703
Alice Jowsey <12345alcatraz@gmail.com>
2020-08-31 12:43:57 +1200

    function fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -1,6 +1,7 @@
 // --- Directions
-// Write a program that console logs the numbers
-// from 1 to n. But for multiples of three print
+// step 1 Write a program that console logs the numbers
+// from 1 to n. 
+//But for multiples of three print
 // “fizz” instead of the number and for the multiples
 // of five print “buzz”. For numbers which are multiples
 // of both three and five print “fizzbuzz”.
@@ -12,6 +13,22 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
 
+    for (let i = 1; i <= n; i++) { //specifically iterate from 1 to n not from 0 
+        //check to see if number a multiple of 3 & 5 if so console log here
+    if (i % 3 === 0 && i % 5 === 0) {
+     console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+        //Is the number a multiple of 3?
+        console.log('fizz');
+    } else if (i % 5 === 0) {
+         //Is the number a multiple of 3?
+        console.log('buzz')
+    } else {
+         //Is the number a multiple of 3/5 or 15 if not:
+        console.log(i)
+    }
+ }
+}
 module.exports = fizzBuzz;
