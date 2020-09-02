70904a3c0313ccccc6f50008ea771e0566af5e97
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-02 16:14:40 +1200

    function steps iteration sol1

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,31 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//solution 1 psuedo code
+//first I need rows row 1 row 2 row 3
+//I need columns 1/2/3
+// FOR loop iterates through rows from 0 to n
+//create an empty string called 'stair'
+//second FOR loop iterates through columns left to right
+//If the current col is equal to or less than current row add a # to the stair
+// ELSE add a space to the stair
+//running through for loop and iterate through col and console log stair
+// the stair variable is declared as a new empty string in the continued iterations
+
+function steps(n) {
+for (let row = 0; row < n; row++) {
+    let stair = '';
+    
+    //row processing step
+    for (let column = 0; column <n; column++) {
+        if (column <= row) {
+            stair += '#';
+        } else {
+            stair += ' ';
+        }
+    }
+    console.log(stair);
+ }
+}
 
 module.exports = steps;
