b25085268ab1ffe9bb1f8ca0d9af532e73e31c49
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-02 15:43:22 +1200

    function capitalize

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,33 +9,44 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
-
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false; //no anagram one char map has diff type
-  }
-  // if same char ..proceed with map check process > final iterate over both a & b and compare both here
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) {
-      //how many times has a been used inside charM? If not used return false
-      return false;
-    }
-  }
-  return true;
+  return cleanString(stringA) === cleanString(stringB); //compare strings Identical = anagram
 }
 
-function buildCharMap(str) {
-  const charMap = {};
-
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-    //take char map assign key and then increment value at the char > assign value
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-  return charMap;
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
 
 module.exports = anagrams;
+
+// solution 1 option
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharMap = buildCharMap(stringB);
+
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//       return false; //no anagram one char map has diff type
+//     }
+//     // if same char ..proceed with map check process > final iterate over both a & b and compare both here
+//     for (let char in aCharMap) {
+//       if (aCharMap[char] !== bCharMap[char]) {
+//         //how many times has a been used inside charM? If not used return false
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+
+//   function buildCharMap(str) {
+//     const charMap = {};
+
+//     for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//       //take char map assign key and then increment value at the char > assign value
+//       charMap[char] = charMap[char] + 1 || 1;
+//     }
+//     return charMap;
+//   }
+
+//   module.exports = anagrams;
 
 //notes
 // const word = "HI THERE!!!!!"
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,50 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
 
-module.exports = capitalize;
+//solution 2
+//create 'result' which is the first character of the input string capitalized in an empty string (not an array)
+//FOR each char in the string
+// IF the char to the left has space
+//capitalize it and add it to a 'result'
+//ELSE add it to a result
+
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+    
+    for (let i = 1; i< str.length; i++){
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+     } else {
+         result += str[i];
+     }
+   }
+   return result;
+  }
+  
+  module.exports = capitalize;
+
+
+
+
+
+//solution 1 option
+// pseudo code make empty array constant 'words'
+// split the input string by spaces to get an array 
+//for each word in the array 
+//uppercase the first letter of the word
+//join the first letter with the rest of the string
+//push result into words array
+//join 'words' into a string and return it 
+
+// function capitalize(str) {
+//     const words = [];
+
+//     for (let word of str.split(' ')) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+
+//     return words.join(' ');
+// }
+
+// module.exports = capitalize;
