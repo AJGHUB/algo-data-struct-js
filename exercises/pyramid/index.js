a8157bb18586765c0eb62c2143952848c6cfdf79
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-04 11:36:30 +1200

    function pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,33 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    const midpoint = Math.floor((2 * n -1) / 2);
 
+
+    for (let row = 0; row < n; row++) {
+        let level = '';
+
+        for (let column = 0; column < 2 * n -1; column++) { 
+        //iterates through columns, adds together then minus 1 from bottom row ie 4x2=8 -1 =7
+           if(midpoint - row <= column && midpoint + row >= column) {
+        // midpoint checkpoint to make sure check is within some bracket of numbers/bounds of midpoint minus row and midpoint plus row
+               level += '#'; //if condition is met pound is added
+           } else {
+               level += ' ';
+           }
+        }
+        console.log(level)
+    }
+}
 module.exports = pyramid;
+
+// solution 1 
+// from 0 to n (iterate through rows)
+// create an empty string
+// from 0 to ??? (columns)
+// IF the column should have a #
+// Add a pound to level
+// ELSE
+// Add a space to 'level
+//console log 'stair'
