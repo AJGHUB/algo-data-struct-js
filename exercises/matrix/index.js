5c6807f81a595d18ed9bdddde0f68ba44314fe46
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-04 14:39:51 +1200

    function matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,52 +15,60 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {
-    const results = []; //empty array
-
-    for (let i = 0; i < n; i++) { //number of sub arrays in dep on value n iterate
-    results.push([]);   // and push in empty sub array
-    }
-
-    let counter = 1; //counter variable, keeps track of what number im trying to push into array
-    let startColumn = 0;
-    let endColumn = n - 1;
-    let startRow = 0;
-    let endRow = n - 1;
-    while (startColumn <= endColumn && startRow <= endRow) {
-        //Top row: progress through FL i = end then inc 1 with results assign counter and increment start row
-        for (let i = startColumn; i <= endColumn; i++) { //iterate st col > end col
-            results[startRow] [i] = counter;             // put value into result set
-            counter++;
-        }
-        startRow++;  //inc start row
-
-        //Right Column FLoop
-        for (let i = startRow; i <= endRow; i++) {
-            results [i] [endColumn] = counter;
-            counter++; 
-        }
-        endColumn--;
-
-         // Bottom Row FLoop
-         for (let i = endColumn; i >= startColumn; i--) {
-            results[endRow] [i] = counter;
-            counter++; 
-        }
-        endRow--;
-
-        //Start Column Floop
-        for (let i = endRow; i >= startRow; i--) {
-            results[i] [startColumn] = counter;
-            counter++;
-        }
-        startColumn++;
-    } 
-    return results;
-}
+function matrix(n) {}
 
 module.exports = matrix;
 
+
+
+// function matrix(n) {
+//     const results = []; //empty array
+
+//     for (let i = 0; i < n; i++) { //number of sub arrays in dep on value n iterate
+//     results.push([]);   // and push in empty sub array
+//     }
+
+//     let counter = 1; //counter variable, keeps track of what number im trying to push into array
+//     let startColumn = 0;
+//     let endColumn = n - 1;
+//     let startRow = 0;
+//     let endRow = n - 1;
+//     while (startColumn <= endColumn && startRow <= endRow) {
+//         //Top row: progress through FL i = end then inc 1 with results assign counter and increment start row
+//         for (let i = startColumn; i <= endColumn; i++) { //iterate st col > end col
+//             results[startRow] [i] = counter;             // put value into result set
+//             counter++;
+//         }
+//         startRow++;  //inc start row
+
+//         //Right Column FLoop
+//         for (let i = startRow; i <= endRow; i++) {
+//             results [i] [endColumn] = counter;
+//             counter++; 
+//         }
+//         endColumn--;
+
+//          // Bottom Row FLoop
+//          for (let i = endColumn; i >= startColumn; i--) {
+//             results[endRow] [i] = counter;
+//             counter++; 
+//         }
+//         endRow--;
+
+//         //Start Column Floop
+//         for (let i = endRow; i >= startRow; i--) {
+//             results[i] [startColumn] = counter;
+//             counter++;
+//         }
+//         startColumn++;
+//     } 
+//     return results;
+// }
+
+// module.exports = matrix;
+
+
+//SPIRAL of variables for matrix
 //algorithm pseudo code is predicated on the idea of maintaining...
 //....counter variables that reflect what 'start col end col' 'start row and end row' working on 
 //to keep track of index point of current integer values being filled inside matrix (start row starts @ 0 /n depends on end row mutable)
