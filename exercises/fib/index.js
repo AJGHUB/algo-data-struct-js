5ecb4f68e0e098e7ef3dd395b0ee6dfecbf29225
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-07 17:30:29 +1200

    runtime complex notes

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -14,10 +14,25 @@ module.exports = fib;
 
 
 
-//Runtime Complexity notes Different cases: 
+//performance of algoritim; Runtime Complexity notes Different cases: 
 //Constant Time: (1) No matter how many elements we work with, the algorithm / operation / whatever will always take the same amount of time.
 //Logarithmic: (logn) You have this if doubling the number of elements you are iterating over doesnt double the amount of work. Always assume that searching options are log(n)
 //Linear Time (n) Iterating through all elements in a collection of data. If you see a for loop spanning from '0' to array.length'
 //Quasilinear Time (n * log (n)) You have this if doubling the number of elements you are iterating over doesnt double the amount of work. Always assume that any sorting is n*log(n)
 //Quadratic Time (n ^ 2) Every element in a collection has to be compares to every other element. 'The handshake problem'
 //Exponential Time ( 2 ^ n ) If you add a 'single' element to a collection, the processing power required doubles. > use other options to highlight inefficiency than this one
+
+// BIG O NOTATION ~ run time complexity > what is the efficiency of your algorithm
+// O(n) Linear
+// O(1) Constant
+// O(n^2) Quadratic
+
+//Simple for loop iteration through single collection= linear runtime Probably O(n)
+//Iterating through half a collection = Linear O(n) no constants in runtime
+//Iterating through two diff collections with seperate for loops = O(n+m) add a second term arg/ string. seperate them to represent
+//Nested for loops iterating over the same collection (ie pyramid or steps) you have = O(n^m) nsquared runtime
+//Two nested for loops iterating over diff sets of data = O (n*m)
+//sorting? = O(n*log(n))
+//searching a sorted array O(log(n))
+
+//Space complexity: how much more memory is required by doubling the problem set?
