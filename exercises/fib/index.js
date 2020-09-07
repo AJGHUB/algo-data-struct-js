535c2db0cb843b8c85b236d4b37762e0ddf77466
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-07 17:15:05 +1200

    runtime complex notes

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -11,3 +11,13 @@
 function fib(n) {}
 
 module.exports = fib;
+
+
+
+//Runtime Complexity notes Different cases: 
+//Constant Time: (1) No matter how many elements we work with, the algorithm / operation / whatever will always take the same amount of time.
+//Logarithmic: (logn) You have this if doubling the number of elements you are iterating over doesnt double the amount of work. Always assume that searching options are log(n)
+//Linear Time (n) Iterating through all elements in a collection of data. If you see a for loop spanning from '0' to array.length'
+//Quasilinear Time (n * log (n)) You have this if doubling the number of elements you are iterating over doesnt double the amount of work. Always assume that any sorting is n*log(n)
+//Quadratic Time (n ^ 2) Every element in a collection has to be compares to every other element. 'The handshake problem'
+//Exponential Time ( 2 ^ n ) If you add a 'single' element to a collection, the processing power required doubles. > use other options to highlight inefficiency than this one
