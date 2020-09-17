6679a40c24c76edaab60808abd1b2445a0a984f3
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-17 14:27:40 +1200

    linked list insert first method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -3,22 +3,32 @@
 // See 'directions' document
 
 class Node {
-    constructor(data, next = null) {
-        this.data = data;
-        this.next = next;
-    } //node class api put together 
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  } //node class api put together
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
-    }    
-}
+  constructor() {
+    this.head = null;
+  }
+  insertFirst(data) {
+    this.head = new node(data, this.head);
+  }
+} //Test constructor A Node in appends a node to the start of the list
 
 module.exports = { Node, LinkedList };
 
 
 
-//A node is a basic unit of a data structure, such as a linked list or tree data structure. 
+
+
+
+
+
+
+
+//A node is a basic unit of a data structure, such as a linked list or tree data structure.
 //Nodes contain data and also may link to other nodes.
 // Links between nodes are often implemented by pointers.
