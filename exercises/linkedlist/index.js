91e8a780a8fde1d636a6ba8e61348f55b74461b3
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-19 16:25:12 +1200

    linked list remove first

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -50,6 +50,12 @@ class LinkedList {
   clear() {
     this.head = null
   }
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next
+  }
 }
 
 module.exports = { Node, LinkedList };
