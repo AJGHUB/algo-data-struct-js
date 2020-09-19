8671b1247270cb86c14ca1c81a449401648058b0
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-19 18:48:31 +1200

    linked list get at solution

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -88,6 +88,22 @@ class LinkedList {
     }
   }
 
+  getAt(index) {
+   
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+    if (counter === index){
+      return node;
+    }
+
+
+    counter++;
+    node=node.next;
+  }
+  return null;
+}
+
 }
 module.exports = { Node, LinkedList };
 
