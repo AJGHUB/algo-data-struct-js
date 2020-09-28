db7e30c6ee5c021950c9834e91521fa16673dc80
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-28 19:41:57 +1300

    building a tree

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -17,8 +17,7 @@ class Node {
     }
 
     add (data) {
-        const node = new Node(data);
-        this.children.push(node);
+        this.children.push(new Node(data));
     }
 
     remove(data) {
@@ -26,10 +25,35 @@ class Node {
             return node.data !==data;
         }); //equals data or removes it
     }
-
-
 }
 
-class Tree {}
+
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    traverseBF(fn) {
+        const arr = [this.root];
+        while (arr.length) {
+            const node = arr.shift(); //shift built in arr method removes 1st element of arr 
+            
+            arr.push(...node.children); //'...'es 2015 spread op take elements push one by one into arr(same as for loop)
+           fn(node);
+        }    
+    }
+
+    traverseDF(fn) {
+        const arr = [this.root];
+        while(arr.length) {
+            const node = arr.shift();
+
+            arr.unshift(...node.children); //unshift built in array method takes element add onto front of arr
+            fn(node);
+        }
+    }
+}
+
 
 module.exports = { Tree, Node };
