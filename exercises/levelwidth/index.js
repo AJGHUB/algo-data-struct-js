0c53319061c9d74d747ae9774ce4d2e0efffaa4f
Alice Jowsey <12345alcatraz@gmail.com>
2020-09-29 18:23:29 +1300

    tree level width function

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,29 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr = [root, 's'];
+    const counters = [0];
+
+ while (arr.length > 1) {
+     const node = arr.shift();
+
+     if (node === 's') {
+         counters.push(0);
+         arr.push('s');
+     } else {
+         arr.push(...node.children)
+         counters [counters.length - 1] ++;
+     }
+ }
+
+ return counters;
+}
 
 module.exports = levelWidth;
+
+
+// PASS  levelwidth/test.js
+//   √ levelWidth is a function (2 ms)
+//   √ levelWidth returns number of nodes at widest point (1 ms)
+//   √ levelWidth returns number of nodes at widest point
