283e9affbbb25cab741b8c15885695864e6abb04
Alice Jowsey <12345alcatraz@gmail.com>
2020-10-01 13:59:43 +1300

    binary search tree

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -10,28 +10,28 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
+//use BF or Df manually with tree
 
 function levelWidth(root) {
-    const arr = [root, 's'];
-    const counters = [0];
+  const arr = [root, 's'];
+  const counters = [0];
 
- while (arr.length > 1) {
-     const node = arr.shift();
+  while (arr.length > 1) {
+    const node = arr.shift();
 
-     if (node === 's') {
-         counters.push(0);
-         arr.push('s');
-     } else {
-         arr.push(...node.children)
-         counters [counters.length - 1] ++;
-     }
- }
+    if (node === 's') {
+      counters.push(0);
+      arr.push('s');
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
 
- return counters;
+  return counters;
 }
 
 module.exports = levelWidth;
-
 
 // PASS  levelwidth/test.js
 //   √ levelWidth is a function (2 ms)
