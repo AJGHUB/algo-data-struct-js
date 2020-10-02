fd706fff268db6ff51cc6d7fd33808d5f02d019b
Alice Jowsey <12345alcatraz@gmail.com>
2020-10-02 17:21:51 +1300

    sorting with merge mergesort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,6 +1,11 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+
+
+//______________________________________BUBBLE SORT______________________________________________________________
+
+
 function bubbleSort(arr) {
   //Implement bubblesort
   for (let i = 0; i < arr.length; i++) {
@@ -21,6 +26,11 @@ function bubbleSort(arr) {
   return arr;
 }
 
+
+
+//______________________________________SELECTION SORT__________________________________________________________
+
+
 function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
     let indexOfMin = i; //lowest value in array prove me wrong
@@ -37,12 +47,60 @@ function selectionSort(arr) {
         arr[i] = lesser;
     }
     }
-    
+
     return arr;
 }
 
-function mergeSort(arr) {}
 
-function merge(left, right) {}
 
-module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+//______________________________________MERGE SORT_____________________________________________________________
+
+
+function mergeSort(arr) {                       //split arr into 2 
+    if (arr.length === 1) {                     //check does array has one element inside it . return immed 
+        return arr;
+    }
+    
+    
+    //divide arr into 2 equal half to find centre
+    const center = Math.floor(arr.length /2);
+    const left = arr.slice(0, center);          //index 0 up to 2 not inc 2
+    const right = arr.slice(center);            //take everything from index 2 to end of arr
+ 
+    return merge(mergeSort(left), mergeSort(right));
+
+}
+ 
+
+
+
+//______________________________________MERGE_____________________________________________________________
+
+
+function merge(left, right) {
+
+const results = [];
+
+while(left.length && right.length) {
+    if (left[0] < right[0]) {
+        results.push(left.shift());
+    } else {
+        results.push(right.shift());
+    }
+}
+
+return [...results, ...left, ...right];  //es6 JS concat - join left right and add using spread op
+}
+
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };    
+//consider calling merge sort alg with an array of length 2
+
+
+// Bubble sort
+// √ sorts an array (2 ms)
+// Selection sort
+// √ sorts an array
+// Merge sort
+// √ merge function can join together two sorted arrays (1 ms)     
+// √ sorts an array
