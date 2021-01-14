c119309539e0f1b2a28b47a8075f6842036cbb0e
Alice Jowsey <12345alcatraz@gmail.com>
2021-01-14 16:57:43 +1300

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,14 +8,25 @@
 //   palindrome("abcdefg") === false
 
 
-//  solution 2 - not ideal but one way of solving 'an advanced array helper "every" ' - complex in array and mirroring & access to array;
- function palindrome(str)  { //turn string to array every helper only has access to an array
-    return str.split('').every((char, i) => {     //str.split makes array | i=0 first element of array 
-        return char === str[str.length - i - 1];  //give access to length -1 of array other end, returns index at other sid eof array mirrored
-    }); 
- }
 
- module.exports = palindrome;
+function palindrome(str) {
+ const reversed = str.split('').reverse().join('');
+ 
+    return str === reversed;
+}
+
+module.exports = palindrome
+
+
+
+//  solution 2 - not ideal but one way of solving 'an advanced array helper "every" ' - complex in array and mirroring & access to array;
+//  function palindrome(str)  { //turn string to array every helper only has access to an array
+//     return str.split('').every((char, i) => {     //str.split makes array | i=0 first element of array 
+//         return char === str[str.length - i - 1];  //give access to length -1 of array other end, returns index at other sid eof array mirrored
+//     }); 
+//  }
+
+//  module.exports = palindrome;
 
 
 
